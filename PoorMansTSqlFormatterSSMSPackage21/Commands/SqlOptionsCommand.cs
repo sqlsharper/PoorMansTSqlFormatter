148bc2a8012a05b1ca9774e275c23b2ea606feda
Bob Chin <cnsharp@yeah.net>
2025-06-10 14:30:40 +0800

    Support SSMS 21.

---
+++ PoorMansTSqlFormatterSSMSPackage21/Commands/SqlOptionsCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.Shell;
+using PoorMansTSqlFormatterSSMSLib;
+using Task = System.Threading.Tasks.Task;
+
+namespace PoorMansTSqlFormatterSSMSPackage.Commands
+{
+    internal sealed class SqlOptionsCommand
+    {
+        public const int CommandId = 0x101;
+        public static readonly Guid CommandSet = PackageGuids.guidPoorMansTSqlFormatterSSMSVSPackageCmdSet;
+        private readonly AsyncPackage package;
+
+        private SqlOptionsCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new OleMenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            new SqlOptionsCommand(package, commandService);
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var helper = new GenericVSHelper(true, null, null, null);
+            helper.GetUpdatedFormattingOptionsFromUser();
+        }
+    }
+}
---
+++ PoorMansTSqlFormatterSSMSPackage21/FormatterPackage.cs
@@ -0,0 +1,49 @@
+﻿/*
+Poor Man's T-SQL Formatter - a small free Transact-SQL formatting 
+library for .Net 2.0 and JS, written in C#. 
+Copyright (C) 2011-2017 Tao Klerks
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/*
+ * Migrate to support SSMS 18-20, by:Bob Chin(CnSharp Studio), 4/1/2025 (Oh, it's April Fool's Day)
+ */
+
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+
+namespace PoorMansTSqlFormatterSSMSPackage
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [ProvideAutoLoad(UIContextGuids.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.guidPoorMansTSqlFormatterSSMSVSPackagePkgString)]
+    public sealed class FormatterPackage : AsyncPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            await Commands.FormatSqlCommand.InitializeAsync(this);
+            await Commands.SqlOptionsCommand.InitializeAsync(this);
+        }
+    }
+}
